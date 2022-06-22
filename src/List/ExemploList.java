946012e75c3a556d268741f0655f97cd2665dcc6
RaphaelAlcantara <rvfra@discente.ifpe.edu.br>
2022-06-22 11:01:20 -0300

    Exercicios List e LinkedList

--- src/List/ExemploList.java
+++ src/List/ExemploList.java
@@ -1,9 +1,6 @@
 package List;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class ExemploList {
     public static void main(String[] args) {
@@ -19,7 +16,13 @@ public class ExemploList {
         notas.add(3.6);
 
         System.out.println(notas);
-
+//LinkedList
+        LinkedList<Double> notas2 = new LinkedList<>(notas);
+        System.out.println("notas2: "+notas2);
+        System.out.println("primeiro elemento: "+notas2.getFirst());
+        System.out.println("primeira nota removida: "+notas2.removeFirst());
+        System.out.println("notas2: "+notas2);
+//termina linkedList
         System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5.0)); //indexOf retorna a posição da nota
 
         System.out.println("Adicione nota 8.0 na posição 4");
@@ -35,10 +38,10 @@ public class ExemploList {
         //verificar se existe é com contains e retorna boolean
         System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5.0));
 
-//        System.out.println("Imprima as notas uma abaixo da outra");
-//        for (Double nota : notas){
-//            System.out.println(nota);
-//        }
+/*        System.out.println("Imprima as notas uma abaixo da outra");
+        for (Double nota : notas){
+            System.out.println(nota);
+        }*/
 
         System.out.println("Exiba a terceira nota adicionada");
         //get retorna o elemento na posição
@@ -51,6 +54,44 @@ public class ExemploList {
         System.out.println("Exiba a maior nota");
         //metodo max retorna o maior elemento da Collection
         System.out.println("Maior nota: " + Collections.max(notas));
+
+       System.out.println("Exiba a soma das notas");
+        //interator serve para percorrer a Collection elemento a elemento
+        double soma = 0;
+        Iterator<Double> iterator = notas.iterator();
+        while (iterator.hasNext()){ //Enquanto existir proximo elemento
+            soma += iterator.next();//adiciona o elemento na soma
+        }
+        System.out.println("Soma: " + soma);
+
+        System.out.println("Exiba a media das notas");
+
+        double media = soma / notas.size();
+        System.out.println("Media: " + media);
+
+        System.out.println("remova a nota 0");
+        notas.remove(0.0);
+        System.out.println(notas);
+
+        System.out.println("remova a nota da posição 0");
+        notas.remove(notas.get(0));
+        System.out.println(notas);
+
+        System.out.println("remova as notas menores que 7");
+        notas.removeIf(nota -> nota < 7);
+        System.out.println(notas);
+
+        System.out.println("apague toda a lista");
+        notas.clear();
+        System.out.println(notas);
+
+        System.out.println("verifique se a lista é vazia");
+        if(notas.isEmpty()){
+            System.out.println("Lista vazia");
+        }else{
+            System.out.println("Lista não vazia");
+        }
+
 
     }
 }
