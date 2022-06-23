a0c827e1536c078304bcbf86bd3a45f092b5fc99
RaphaelAlcantara <rvfra@discente.ifpe.edu.br>
2022-06-23 14:59:16 -0300

    exercicio1 realizado

---
+++ src/List/exercicio1.java
@@ -0,0 +1,43 @@
+package List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    Faça um programa que receba a temperatura média dos
+    6 primeiros meses do ano e armazene em uma lista.
+    Após isto, calcule a média semestral das temperaturas
+    e mostre todas as temperaturas acima desta média, e em
+    que mês ocorreram pelo valor do mês.
+
+*/
+public class exercicio1 {
+    public static void main(String[] args) {
+        List<Double> temperaturas = new ArrayList<>();
+
+        temperaturas.add(23.0);
+        temperaturas.add(26.5);
+        temperaturas.add(24.3);
+        temperaturas.add(26.4);
+        temperaturas.add(26.0);
+        temperaturas.add(26.6);
+
+        double soma = 0;
+        for (Double temperatura : temperaturas) {
+            soma += temperatura;
+        }
+        double media = soma / temperaturas.size();
+        System.out.println("Média: " + media);
+
+
+        System.out.println("Temperaturas acima da média: " + temperaturas);
+
+        for(int i = 0; i < temperaturas.size(); i++){
+            if(temperaturas.get(i) > media){
+                System.out.println("Mês " + i + " - " + temperaturas.get(i));
+            }
+        }
+
+
+    }
+}
