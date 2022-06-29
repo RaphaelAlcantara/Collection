663e6dca517b0d33173c4ad89eb4c8931c857c7e
RaphaelAlcantara <rvfra@discente.ifpe.edu.br>
2022-06-29 11:39:48 -0300

    Ex1.java iniciado

---
+++ src/Map/Ex1.java
@@ -0,0 +1,28 @@
+package Map;
+/*
+Dada a população estumada de alguns estados do NE, faça:
+•Estado = PE - População = 9.616.621
+•Estado = AL - População = 3.351.543
+•Estado = CE - População = 9.187.103
+•Estado = RN - População = 3.534.265
+
+a) Crie um dicionário e relacione os estados com as suas populações;
+b) Substitua a população do estado do RN por 3.534.165;
+c) Confira se o estado de PB existe, caso não adicione: PB - 4.039.277;
+d) Exiba a população de PE;
+e) Exiba todos os estados e as suas populações na ordem de inseridos;
+f) Exiba os estados com população na ordem alfabética;
+g) Exiba o estado com o menor número de habitantes e a sua quantidade;
+h) Exiba o estado com o maior número de habitantes e a sua quantidade;
+i) Exiba a soma das populações dos estados;
+j) Exiba a média das populações dos estados;
+k) Remova os estados com população menor que 4.000.000;
+l) Apague o dicionário;
+m) Confira se o dicionário está vazio;
+
+ */
+public class Ex1 {
+    public static void main(String[] args) {
+
+    }
+}
